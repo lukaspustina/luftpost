3e66ab73651d048bb438da77dfa52433803d7bb2
Lukas Pustina <lukas@pustina.net>
2017-06-19 18:53:14 +0200

    Add initial output

---
+++ src/output.rs
@@ -0,0 +1,20 @@
+use measurements::{Measurement, ValueType};
+use std::io::Write;
+use tabwriter::TabWriter;
+
+pub fn output(measurements: &[Measurement]) -> () {
+    let mut tw = TabWriter::new(vec![]);
+    for ref m in measurements {
+        let _ = writeln!(
+            &mut tw,
+            "Name is missing\t({})\tSDS_P1: {}",
+            m.software_version,
+            m.data_values.get(&ValueType::SDS_P1).unwrap_or(&-1.0f32),
+        );
+    }
+    tw.flush().unwrap();
+    let out_str = String::from_utf8(tw.into_inner().unwrap()).unwrap();
+
+    println!("{}", out_str);
+}
+
